87db9eaac513981cb1e72aa3978e9fcaba004a92
Timmy Kokke <mental_c@hotmail.com>
2020-11-18 21:27:48 +0100

    updated todo, tuning speeds

--- src/components/game.component.js
+++ src/components/game.component.js
@@ -4,30 +4,88 @@ AFRAME.registerComponent('game', {
    init: function () {
       this.world = document.getElementById('world');
       this.rotation = 0;
-      for (let i = 0; i < 250; i++) {
-         const coneRoot = document.createElement('a-entity')
-         const cone = document.createElement('a-entity');
-         cone.setAttribute('geometry', 'primitive: cone; radiusBottom: 1; radiusTop: 0;height:5');
-         cone.setAttribute('position', '0 20 0');
-         cone.setAttribute('pixel-material','index:0;repeat:5 10;')
-         coneRoot.appendChild(cone);
-         coneRoot.setAttribute('rotation', { x: Math.random() * 360, y: 0, z: Math.random() * 360 });
-         this.world.appendChild(coneRoot);
+
+      this.lefthand = document.getElementById('left-hand');
+      this.righthand = document.getElementById('right-hand');
+
+      for (let i = 0; i < 500; i++) {
+         const z = Math.random() * 360;
+         if ((z > 5 && z < 175)|| (z > 185 && z < 355)) {
+            const coneRoot = document.createElement('a-entity')
+            const cone = document.createElement('a-entity');
+            cone.setAttribute('geometry', `primitive: cone; radiusBottom: 1; radiusTop: 0; height:${Math.random()*4+3}`);
+            cone.setAttribute('position', '0 20 0');
+            cone.setAttribute('pixel-material', 'index:0;repeat:5 10;')
+            coneRoot.appendChild(cone);
+            coneRoot.setAttribute('rotation',
+               {
+                  x: Math.random() * 180,
+                  y: 0,
+                  z: z
+               });
+            this.world.appendChild(coneRoot);
+         }
       }
+      this.speed = 0;
+      this.prevDif = 0;
+      this.jumpHeight = 0;
+      this.deltaJump = 0;
+      this.isReadyToJump = true;
    },
    update: function (oldData) { },
    tick: function (time, timeDelta) {
-      this.world.setAttribute('rotation', { x: this.rotation })
-      const rotDelta = timeDelta * .005;
-      this.rotation = (this.rotation + rotDelta) % 360;
+      if (timeDelta === 0) return;
+      this.world.setAttribute('rotation', { x: this.rotation });
+      this.world.setAttribute('position', { y: -20 - this.jumpHeight });
+      this.rotation = (this.rotation + this.speed) % 360;
+      this.updateSpeed(timeDelta);
 
    },
    tock: function (time, timeDelta, camera) { },
    remove: function () { },
    pause: function () { },
    play: function () { },
-   updateSchema: function (data) { }
+   updateSchema: function (data) { },
+
+   updateSpeed: function (timeDelta) {
+      const ly = this.lefthand.object3D.position.y;
+      const ry = this.righthand.object3D.position.y;
+      const dif = (ly - ry);
+      const newSpeed = Math.abs(this.prevDif - dif) / timeDelta * 100;
+      let slowdownSpeed = 0.002;
+      const speedupspeed = 0.05;
+      const maxspeed = 1;
+      const jumpspeed = .1;
+      const fallspeed = .35;
+      if (!this.isReadyToJump) {
+         slowdownSpeed = 0;
+      }
+      this.speed = newSpeed < this.speed ? Math.max(this.speed - slowdownSpeed, 0) : Math.min(Math.min(this.speed + speedupspeed, newSpeed), maxspeed);
+      this.prevDif = dif;
+
+      const avg = (ly + ry) / 2;
+      const deltaAvg = avg - this.prefAvg
+
+      // todo: prevent double/tripple/etc jump
+      if (this.isReadyToJump) {
+         if (deltaAvg > .02) { // moving Up;         
+            this.deltaJump += jumpspeed//jump
+         } else {
+            this.isReadyToJump = false;
+         }
+      } else {
+         this.deltaJump = Math.max(0, this.deltaJump - jumpspeed*2);
+      }
+      this.jumpHeight += this.deltaJump;
+      this.jumpHeight = Math.max(0, this.jumpHeight - fallspeed);
+      if (this.jumpHeight === 0) {
+         this.isReadyToJump = true;
+      }
+      this.prefAvg = avg;
+   }
 });
+
+
 
 
 
